a059cd088564faeefb689d2850f8cc76201cffb1
Zach Moodie <dev@zachmoodie.com>
2026-06-02 12:45:52 -0600

    Fixing SW error

--- public/sw.js
+++ public/sw.js
@@ -4,6 +4,7 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') return;
   event.respondWith(fetch(event.request));
 });
 
